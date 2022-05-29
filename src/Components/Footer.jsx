7253465f32436852e65b3cfe2e6e3ef15b8afae7
Quentin <q.lambkin@gmail.com>
2022-05-29 14:36:03 -0400

    Add Files

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+    const date = new Date();
+    const currentYear = date.getFullYear();
+
+    return <footer>
+        <p className="footer">Copyright QLambkin {currentYear}</p>
+    </footer>;
+}
+
+export default Footer;
